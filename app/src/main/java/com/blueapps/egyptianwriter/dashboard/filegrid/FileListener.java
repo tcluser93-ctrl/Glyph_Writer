69e45e2067fde32dfe7d5e7c188c60120cc9b2c7
frankltr <146444799+frankltr@users.noreply.github.com>
2026-07-04 23:31:30 +0200

    chore: remove hieroglyphic code — filegrid/FileListener

--- app/src/main/java/com/blueapps/egyptianwriter/dashboard/filegrid/FileListener.java
+++
@@ -1,9 +0,0 @@
-package com.blueapps.egyptianwriter.dashboard.filegrid;
-
-public interface FileListener {
-
-    void OnDeleteFile(String name);
-    void OnExportFile(String name);
-    void OnOpenFile(String name);
-
-}
